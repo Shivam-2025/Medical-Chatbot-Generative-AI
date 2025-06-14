b1e42a7e1a1b77cf2a1f27e12dd5091de6100963
Shivam Yadav <2004shivamyadv@gmail.com>
2025-06-15 01:33:47 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "Generative AI Project",
+    version = '0.0.0',
+    author = 'Shivam Yadav',
+    author_email = '2004shivamyadv@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
